f7180652cd3e1045490af65f85a8f1e6eb2e33e4
hikaru endo <hika.blue91@gmail.com>
2018-10-10 19:57:52 +0900

    website

--- MyProject/MyProject/urls.py
+++ MyProject/MyProject/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from mysite.views import *
+from django.urls import path,include
+#from mysite.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', index)
+    path('', include('mysite.urls'))
 ]
--- MyProject/mysite/views.py
+++ MyProject/mysite/views.py
@@ -3,5 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse('hello world')
+    return render(request, 'mysite/index.html')
+
+def about(request):
+    return render(request, 'mysite/about.html')
+
+def contact(request):
+    return render(request, 'mysite/contact.html')
+
+def home(request):
+    return render(request, 'mysite/home.html')
